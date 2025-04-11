1d4d446c7c8cf9c7ba66875c37b892dc17a88a93
stevenayu <wentao.zhang@pingcap.com>
2025-04-11 23:21:25 +0800

    add test demo pic

--- demo/demo.py
+++ demo/demo.py
@@ -1,14 +1,23 @@
+import os
 from paddlex import create_pipeline
 
 pipeline = create_pipeline(pipeline="OCR")
 
-output = pipeline.predict(
-    input="./demo1.jpg",
-    use_doc_orientation_classify=False,
-    use_doc_unwarping=False,
-    use_textline_orientation=False,
-)
-for res in output:
-    res.print()
-    res.save_to_img(save_path="./output/")
-    res.save_to_json(save_path="./output/")
+pic_dir = "./pic"
+res_idr = "./output"
+os.makedirs(pic_dir, exist_ok=True)
+
+
+for pic in os.listdir(pic_dir):
+    if pic.endswith(".jpg") or pic.endswith(".png"):
+        output = pipeline.predict(
+            input=os.path.join(pic_dir, pic),
+            use_doc_orientation_classify=False,
+            use_doc_unwarping=False,
+            use_textline_orientation=False,
+        )
+        for res in output:
+            res.print()
+            res.save_to_img(save_path="./output/")
+            res.save_to_json(save_path="./output/")
+            print(res.json['res']['rec_texts'])
